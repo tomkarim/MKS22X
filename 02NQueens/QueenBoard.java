e861e19c183c3c7b28177e96ba69e5e0b2dc549e
Tom Karim <tomkarim6@gmail.com>
2018-02-12 17:36:53 -0500

    added addqueen and remove queen, will tesrt soon

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,7 +1,39 @@
 public class QueenBoard{
     private int[][] board; 
 
-    public QueenBoard(int size){
-	board = new int[size][size];
-	
+    public QueenBoard(int size) {
+        board = new int[size][size];
+    }
+
+    private boolean addQueen(int r, int c){
+        for (int y = 0; y < board.length; y++){
+            for (int x = 0; x < board.length; x++){
+                int xd = c - x;
+                int yd = r - y;
+                if(Math.abs(xd*yd) == 0 || Math.abs(xd*yd) == xd * yd){
+                    board[y][x] += 1;
+                }
+            }
+        }
+        board[r][c] = -1;
+    }
+
+    private void removeQueen(int r, int c){
+        for (int y = 0; y < board.length; y++){
+            for (int x = 0; x < board.length; x++){
+                int xd = c - x;
+                int yd = r - y;
+                if(Math.abs(xd*yd) == 0 || Math.abs(xd*yd) == xd * yd){
+                    board[y][x] -= 1;
+                }
+            }
+        }
+        board[r][c] = 0;
+    }
+
+    
+
+
+
+}
 	

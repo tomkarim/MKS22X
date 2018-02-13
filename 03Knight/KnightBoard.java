9135f9e5092123f6d40cd2a93580a61ac3b31002
Tom Karim <tomkarim6@gmail.com>
2018-02-13 14:39:51 -0500

    array of moves

---
+++ 03Knight/KnightBoard.java
@@ -0,0 +1,15 @@
+public class KnightBoard{
+    private int[][]  board;
+    private int[] moves; //2,1,2,-1,-2,1,-2,-1,1,-2,1,2,-1,-2,-1,2 (row, col)
+
+    public KnightBoard(int startingRows, int startingCols){
+	board = new int[startingRows][startingCols];
+
+
+    }
+
+
+    
+
+
+}

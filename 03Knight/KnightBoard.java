9709c6c833cf78c402a2ab62a064a5f943b4ea3f
Tom Karim <tomkarim6@gmail.com>
2018-02-14 14:38:41 -0500

    added 2d array of moves and isvalidmove boolean

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,10 +1,15 @@
 public class KnightBoard{
     private int[][]  board;
-    private int[] moves; //2,1,2,-1,-2,1,-2,-1,1,-2,1,2,-1,-2,-1,2 (row, col)
+    private int[][]  moves = new int[8][8];
+    //2,1,2,-1,-2,1,-2,-1,1,-2,1,2,-1,-2,-1,2 (row, col)
 
     public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
 
+
+    }
+
+    public boolean isValidMove(){
 
     }
 

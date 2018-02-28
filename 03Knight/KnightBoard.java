5a0e2d92d60188f59f5d098bd7321709392e19ce
Tom Karim <tomkarim6@gmail.com>
2018-02-27 19:34:37 -0500

    fixed errs with using r instead of row, etc

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,14 +1,14 @@
 public class KnightBoard{
     private int[][]  board;
-    private int[]  moves = new int[16];
+   // private int[]  moves = new int[16];
     //2,1,2,-1,-2,1,-2,-1,1,-2,1,2,-1,-2,-1,2 (row, col)
 
     public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
     }
 
-    public boolean isValidMove(){
-
+    public boolean isValidMove(int r, int c){
+        return board[r][c] == 0 && r >= 0 && c >= 0 && r < board.length && c < board[0].length;
     }
 
     public boolean solve(int startingRow, int startingCol){
@@ -17,42 +17,53 @@ public class KnightBoard{
 	}
 	return solveH(startingRow, startingCol, 1);
     }
-		
 
-    public boolean solveH(int row, int col, int level){
-	if (level == 1){
-	    board[0][0] = 1;
-	}
-	
-	if (level == board.length * board[0].length){
-	    return true;
-	}
-	
-	for(int i = 0; i < moves.length; i++){
-	    int r = moves[i];
-	    int c = moves[i+1];
-	    if(isValidMove(r, c)){
-		placeKnight(r, c, level+1);
-		if(solveH(r, c, level+1)){
-		    return true;
-		}
-		else{
-		    removeKnight(r, c);
-		}
-	    }
-	}
+    public boolean solveH(int row, int col, int level) {
+        if (level == 1) {
+            board[0][0] = 1;
+        }
+
+        if (level == board.length * board[0].length) {
+            return true;
+        }
+
+        for (int i = 0; i < moves.length; i++) {
+            int r = moves[i];
+            int c = moves[i + 1];
+            if (isValidMove(r, c)) {
+                placeKnight(r, c, level + 1);
+                if (solveH(r, c, level + 1)) {
+                    return true;
+                } else {
+                    removeKnight(r, c);
+                }
+            }
+        }
+        return false;
     }
 
     public boolean placeKnight(int row, int col, int level){
-
+        board[row][col] = level;
+        return isValidMove(row, col);
     }
 
     public boolean removeKnight(int row, int col){
-	
+        board[row][col] = 0;
+        return isValidMove(row, col);
     }
 
-    public string toString(){
-
+    public String toString(){
+        String s = "";
+        for(int r = 0; r < board.length; r++){
+            for(int c = 0; c < board[0].length; c++){
+                if(board[r][c] <= 9){
+                    s += " ";
+                }
+                s += board[r][c] + " ";
+            }
+            s += "\n";
+        }
+        return s;
     }
 
     

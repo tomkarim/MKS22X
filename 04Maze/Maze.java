86861f4032e5dabfa9c7d2097ab079c08823c228
Tom Karim <tomkarim6@gmail.com>
2018-03-01 14:33:25 -0500

    recloned

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -5,7 +5,7 @@ public class Maze{
     private char[][]maze;
     private boolean animate;
 
-    public Maze(String fileName){
+    public Maze(String fileName) throws FileNotFoundException{
 	animate = false;
 	try {
 	    Scanner in = new Scanner(new File(fileName));
@@ -14,9 +14,32 @@ public class Maze{
 
 	    while (in.hasNext()) {
 		text += in.nextLine();
-
 		}
-		
+	    maze = new char[text.length()/n][n];
 
+	    for(int i = 0; i < text.length(); i++){
+		maze[i/n][i%n] = text.charAt(i);
+	    }
+	}
+    }
+   
+    private void wait(int millis){
+         try {
+             Thread.sleep(millis);
+         }
+         catch (InterruptedException e) {
+         }
+     }
+
+      public void setAnimate(boolean b){
+        animate = b;
+    }
+
+    public void clearTerminal(){
+        //erase terminal, go to top left of screen.
+        System.out.println("\033[2J\033[1;1H");
+    }
+
+	       
 	
     }

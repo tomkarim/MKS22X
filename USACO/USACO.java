9f218d0ed77e2a64a507fcc3d370386f1808ce9c
Tom Karim <tomkarim6@gmail.com>
2018-03-07 14:42:07 -0500

    bronze

---
+++ USACO/USACO.java
@@ -0,0 +1,15 @@
+public class USACO{
+    public int[][] pasture;
+    public int[] lakeLevel;
+
+    public void stomp(int r, int c, int d){
+
+    }
+
+    public int[] getMax(int r, int c){
+	int max = pasture[r][c];
+	int row = r;
+	int col = c;
+	
+
+    }

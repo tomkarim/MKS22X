76f02b405ecfe55fbf44824f9f572b4884cfb187
Tom Karim <tomkarim6@gmail.com>
2018-03-07 19:05:56 -0500

    testing changes

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -7,27 +7,36 @@ import java.io.*;
         public Maze(String fileName) throws FileNotFoundException {
             try {
                 Scanner in = new Scanner(new File(fileName));
-                String text = in.nextLine();
-                int len = text.length();
+                String text = "";
+                String counter = in.nextLine();
+                int rows = 0;
 
                 while (in.hasNext()) {
                     text += in.nextLine();
+                    counter += in.nextLine();
+                    rows++;
                 }
 
-                maze = new char[text.length() / len][len];
+                maze = new char[rows][text.length()];
 
-                for (int i = 0; i < text.length(); i++) {
-                    maze[i / len][i % len] = text.charAt(i);
+                Scanner inc = new Scanner(new File(fileName));
+                int r = 0;
+                while(inc.hasNextLine()) {
+                    String textc = inc.nextLine();
+                    for(int c = 0; c < textc.length(); c++){
+                        maze[r][c] = textc.charAt(c);
+                    }
+                    r++;
                 }
 
-                int S = text.length() - text.length() - text.replace("S", "").length();
-                int E = text.length() - text.length() - text.replace("E", "").length();
+                int S = counter.length() - counter.length() - counter.replace("S", "").length();
+                int E = counter.length() - counter.length() - counter.replace("E", "").length();
 
                 if (S != E) {
                     throw new IllegalStateException("There must only be 1 S and one E!");
                 }
-            } catch (FileNotFoundException f) {
-                f.printStackTrace();
+            } catch (FileNotFoundException fnf) {
+                fnf.printStackTrace();
             }
         }
 
@@ -59,32 +68,32 @@ import java.io.*;
                     }
                 }
             }
-            //   maze[startR, startC] == ' ';
-            return solve(startR, startC, 0) - 1;
+            maze[startR][startC] = ' ';
+            return solve(startR, startC, 0);
         }
 
-        private int solve(int r, int c, int count) {
-            if (maze[r][c] == 'E') {
+        private int solve(int row, int col, int count) {
+            if (maze[row][col] == 'E') {
                 return count;
             }
 
             for (int i = 1; i <= 4; i++) {
-                int rr = (i % 2) * (2 - i);
-                int cc = ((i + 1) % 2) * (i - 3);
+                int r = (i % 2) * (2 - i);
+                int c = ((i + 1) % 2) * (i - 3);
 
-                if (isValid(r + rr, c + cc)) {
-                    maze[r][c] = '@';
-                    if (solve(c + cc, r + rr) != -1) {
-                        return solve(c + cc, r + rr);
+                if (isValidMove(row + r, col + c)) {
+                    maze[row][col] = '@';
+                    if (solve(col + c, row + r, count++) != -1) {
+                        return solve(col + c, row + r, count++);
                     }
                 }
             }
 
-            maze[r][c] = '.';
+            maze[row][col] = '.';
             return -1;
         }
 
-        private boolean isValid(int r, int c) {
+        private boolean isValidMove(int r, int c) {
             return maze[r][c] == ' ' || maze[r][c] == 'E';
         }
     }

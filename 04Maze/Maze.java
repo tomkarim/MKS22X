eb47dadda0bd09ec4cd0edebf33f20ae687ad6bf
Tom Karim <tomkarim6@gmail.com>
2018-03-07 20:38:15 -0500

    fixing structure is extremely tedious

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -2,41 +2,41 @@ import java.util.*;
 import java.io.*;
     public class Maze {
         private char[][] maze;
+        private int[][] moves = {{0, 1}, {-1, 0}, {1, 0}, {0, -1}};
         private boolean animate;
 
         public Maze(String fileName) throws FileNotFoundException {
             try {
                 Scanner in = new Scanner(new File(fileName));
                 String text = "";
-                String counter = in.nextLine();
                 int rows = 0;
 
-                while (in.hasNext()) {
-                    text += in.nextLine();
-                    counter += in.nextLine();
+                while (in.hasNextLine()) {
+                    text = in.nextLine();
                     rows++;
                 }
 
                 maze = new char[rows][text.length()];
+                int r = 0;
 
                 Scanner inc = new Scanner(new File(fileName));
-                int r = 0;
-                while(inc.hasNextLine()) {
+                while (inc.hasNextLine()) {
                     String textc = inc.nextLine();
-                    for(int c = 0; c < textc.length(); c++){
+                    // for (int r = 0; r < maze.length; r++) {
+                    for (int c = 0; c < maze[0].length; c++) {
                         maze[r][c] = textc.charAt(c);
                     }
                     r++;
                 }
 
-                int S = counter.length() - counter.length() - counter.replace("S", "").length();
-                int E = counter.length() - counter.length() - counter.replace("E", "").length();
+                //int S = counter.length() - counter.length() - counter.replace("S", "").length();
+                //int E = counter.length() - counter.length() - counter.replace("E", "").length();
 
-                if (S != E) {
-                    throw new IllegalStateException("There must only be 1 S and one E!");
-                }
-            } catch (FileNotFoundException fnf) {
-                fnf.printStackTrace();
+                //if (S != E) {
+                  //  throw new IllegalStateException("There must only be 1 S and one E!");
+               //}
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
         }
 
@@ -68,6 +68,7 @@ import java.io.*;
                     }
                 }
             }
+
             maze[startR][startC] = ' ';
             return solve(startR, startC, 0);
         }
@@ -76,20 +77,20 @@ import java.io.*;
             if (maze[row][col] == 'E') {
                 return count;
             }
+            for (int i = 0; i < 4; i++) {
+                int r = row + moves[i][0];
+                int c = col + moves[i][1];
 
-            for (int i = 1; i <= 4; i++) {
-                int r = (i % 2) * (2 - i);
-                int c = ((i + 1) % 2) * (i - 3);
-
-                if (isValidMove(row + r, col + c)) {
+                if (isValidMove(r, c)) {
                     maze[row][col] = '@';
-                    if (solve(col + c, row + r, count++) != -1) {
-                        return solve(col + c, row + r, count++);
+                    int bool = solve(r, c, count + 1);
+                    if(bool == -1){
+                        maze[row][col] = '.';
+                    } else {
+                        return bool;
                     }
                 }
             }
-
-            maze[row][col] = '.';
             return -1;
         }
 

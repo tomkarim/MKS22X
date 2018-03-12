ba3e7225e00f6960e4c086e6164554e91c0f55bb
Tom Karim <tomkarim6@gmail.com>
2018-03-11 21:35:42 -0400

    edited and almost completed bronze, will test at end

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,15 +1,83 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
     public int[][] pasture;
-    public int[] lakeLevel;
+    public int finalE;
+
+    public static int bronze(String filename) {
+        int[] prm = new int[4];
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        try {
+            Scanner in = new Scanner(new File(filename));
+            pasture = new int[in.nextInt()][in.nextInt()];
+            finalE = in.nextInt();
+            in.nextInt();
+
+            for (int r = 0; r < pasture.length; r++) {
+                for (int c = 0; c < pasture[0].length; c++) {
+                    pasture[r][c] = s.nextInt();
+                }
+            }
+            while (in.hasNextInt()) {
+                stomp(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+
+            return lakeMaker(finalE) * 72 * 72;
+        } catch (FileNotFoundException e) {
+
+        }
+        return 0;
+    }
 
     public void stomp(int r, int c, int d){
+        r--;
+        c--;
 
+        int[] coors = getHighE(r, c);
+        pasture[coors[0]][coors[1]] -= d;
+
+        if(pasture[r][c] < 0){
+            pasture[r][c] = 0;
+        }
+
+        for(int y = r; y < 3; y++){
+            for(int x = c; x < 3; x++){
+                if(pasture[y][x] > pasture[r][c]){
+                    pasture[y][x] = pasture[r][c];
+                }
+            }
+        }
     }
 
-    public int[] getMax(int r, int c){
-	int max = pasture[r][c];
-	int row = r;
-	int col = c;
-	
+    public int[] getHighE(int r, int c){
+        int max = pasture[r][c];
+        int row = r;
+        int col = c;
 
+        for(int y = r; y < 3; y++){
+            for(int x = c; x < 3; x++){
+                if(pasture[y][x] > max){
+                    max = pasture[y][x];
+                    row = y;
+                    col = x;
+                }
+            }
+        }
+        int[] coors = {row, col};
+        return coors;
     }
+
+    public int lakeMaker(int finalE) {
+        int counter = 0;
+        for (int r = 0; r < pasture.length; r++) {
+            for (int c = 0; c < pasture[0].length; c++) {
+                if (pasture[r][c] < finalE) {
+                    counter += finalE - pasture[r][c];
+                }
+            }
+        }
+        return counter;
+    }
+
+}

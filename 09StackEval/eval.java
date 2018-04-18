b9e975bdeb42898dea8fe4c9b230c458751fd717
Tom Karim <tomkarim6@gmail.com>
2018-04-17 21:03:13 -0400

    messed around w it, problem with editing string

--- 09StackEval/eval.java
+++ 09StackEval/eval.java
@@ -1,27 +1,41 @@
 public class eval {
 
 	public static double eval(String str){
+		System.out.println(str.charAt(0));
+		System.out.println(str.charAt(1));
+		System.out.println(str.charAt(2));
+		System.out.println(str.charAt(3));
+		System.out.println(str.charAt(4));
+
 		double sum = 0;
-		for (int i = 4; i < str.length(); i++) {
-			switch (str.charAt(i)) {
-				case '+':
-					sum += str.charAt(0) + str.charAt(2);
-					str = str.substring(4);
-					break;
-				case '-':
-					sum += str.charAt(0) - str.charAt(2);
-					str = str.substring(4);
-					break;
-				case '*':
-					sum += str.charAt(0) * str.charAt(2);
-					str = str.substring(4);
-					break;
-				case '/':
-					sum += str.charAt(0) / str.charAt(2);
-					str = str.substring(4);
-					break;
+		for (int i = 0; i < str.length(); i++) {
+			if(str.){
+				return sum;
+			}
+
+			if (str.charAt(4) == '+') {
+				sum += (str.charAt(0) + str.charAt(2));
+				str = str.substring(4);
+			}
+			if (str.charAt(i) == '-') {
+				sum += str.charAt(0) - str.charAt(2);
+				str = str.substring(i++);
+			}
+			if (str.charAt(i) == '*') {
+				sum += str.charAt(0) * str.charAt(2);
+				str = str.substring(i++);
+			}
+			if (str.charAt(i) == '/') {
+				sum += str.charAt(0) / str.charAt(2);
+				str = str.substring(i++);
 			}
 		}
 		return sum;
 	}
+
+	public static void main(String[] args) {
+		System.out.println(eval("2 1 +"));
+	}
 }
+
+

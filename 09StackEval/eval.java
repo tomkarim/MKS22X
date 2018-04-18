a6e4d0ec72b6ff3d642b98ab4719cc9bf4d12fd4
Tom Karim <tomkarim6@gmail.com>
2018-04-17 20:18:03 -0400

    tried eval using switch for convenience, only testing will tell if that was stupid

--- 09StackEval/eval.java
+++ 09StackEval/eval.java
@@ -1,11 +1,27 @@
-public class eval{
+public class eval {
 
-    public static double(String str){
-	double sum = 0;
-	for(int i = 0; i < str.length(); i++){
-	    if(str.substring(4
-
+	public static double eval(String str){
+		double sum = 0;
+		for (int i = 4; i < str.length(); i++) {
+			switch (str.charAt(i)) {
+				case '+':
+					sum += str.charAt(0) + str.charAt(2);
+					str = str.substring(4);
+					break;
+				case '-':
+					sum += str.charAt(0) - str.charAt(2);
+					str = str.substring(4);
+					break;
+				case '*':
+					sum += str.charAt(0) * str.charAt(2);
+					str = str.substring(4);
+					break;
+				case '/':
+					sum += str.charAt(0) / str.charAt(2);
+					str = str.substring(4);
+					break;
+			}
+		}
+		return sum;
 	}
-
-    }
 }

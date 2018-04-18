5ea23c4db6e044ccbd7d930eab505de1b8af73ab
Tom Karim <tomkarim6@gmail.com>
2018-04-18 14:38:50 -0400

    used scanner to go thru og string, made new str of opers, seems to work so far, will do further testingf

---
+++ 09StackCalc/Calculator.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Calculator{
+
+	public static double eval(String s){
+	    Stack<Integer> stack = new Stack<> ();
+		Scanner str = new Scanner(s);
+		
+		while(str.hasNext()){
+		    if(str.hasNextInt()){
+			stack.push(str.nextInt());
+		    }
+		    else{
+			int n1 = stack.pop();
+			int n2 = stack.pop();
+			String oper = str.next();
+
+			if(oper.equals("+")){
+			    stack.push(n1 + n2);
+			}
+			else if(oper.equals("-")){
+			    stack.push(n1 - n2);
+			}
+			else if(oper.equals("*")){
+			    stack.push(n1 * n2);
+			}
+			else{
+			    stack.push(n1 / n2);
+			}
+		    }
+		}
+		return stack.pop();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(eval("2 1 +"));
+	       	System.out.println(eval("2 1 +"));
+		
+	}
+}
+
+

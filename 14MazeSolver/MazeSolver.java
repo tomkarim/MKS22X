7543f1dc7ee9a1f0d51dcedcebb63f43786a971c
Tom Karim <tomkarim6@gmail.com>
2018-05-07 14:00:48 -0400

    maze Solver added

---
+++ 14MazeSolver/MazeSolver.java
@@ -0,0 +1,19 @@
+public class MazeSolver{
+
+    private Maze maze;
+    prviate Frontier frontier;
+
+    public MazeSolver(String mazeText){
+
+    }
+
+    public boolean solve(){
+	return solve(0);
+    }
+
+    public boolean solve(int mode){
+
+    }
+
+    return false;
+}

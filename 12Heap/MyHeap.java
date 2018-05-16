5048a75fb41cd0461d6e1e1dc8b2ebc8eaaa842e
Tom Karim <tomkarim6@gmail.com>
2018-05-16 14:46:46 -0400

    catching up

---
+++ 12Heap/MyHeap.java
@@ -0,0 +1,22 @@
+public class MyHeap{
+
+    private STring[] data = new String[10];
+    private int n = 1;
+    private int size = 0;
+
+    public MyHeap(){
+    }
+
+    public MyHeap(boolean bool){
+	if(!bool){
+	    n = -1;
+	}
+    }
+
+    public void add(String str){
+
+    }
+
+
+    
+}
